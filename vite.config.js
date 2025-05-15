a2579f628aef22a9df7a9edab3acb5b82bd3feb0
Sarik Sadman <kirasnamdas@gmail.com>
2025-05-15 12:50:32 +0600

    production and dev seperated

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  //for production builds:
-  base: '/static/'
-})
+  base: mode === 'production' ? '/static/' : '/',
+}))
